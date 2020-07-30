e9e6c1ee231c33d4d9812101d8c6ee740947c1bc
nnfeng <citiron@hotmail.com>
2020-07-30 19:25:14 +0800

    Add project files.

---
+++ ConsoleAppSerilogNoDI/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.IO;
+
+namespace ConsoleAppSerilogNoDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the appsettings file
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Apply the config to the logger
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            logger.Verbose("This is a verbose statement");
+            logger.Debug("This is a debug statement");
+            logger.Information("This is a info statement");
+            logger.Warning("This is a warning statement");
+            logger.Error(new IndexOutOfRangeException(), "This is an error statement");
+            logger.Fatal(new AggregateException(), "This is an fatal statement");
+        }
+    }
+}
